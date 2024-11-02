8bdec9d87cfa2879d021a145e3795741404b2cb4
trixie <trixiegail.cloma@cit.edu>
2024-11-02 23:45:27 +0800

    modified app.properties

--- DentalManagement/DentalManagement/src/main/java/com/dentalmanagement/DentalManagement/Controller/SuccessfulRoot.java
+++ DentalManagement/DentalManagement/src/main/java/com/dentalmanagement/DentalManagement/Controller/SuccessfulRoot.java
@@ -1,4 +1,13 @@
 package com.dentalmanagement.DentalManagement.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SuccessfulRoot {
+
+    @GetMapping("/")
+    public String home() {
+        return "Congrats potangina mo successful!";
+    }
 }
