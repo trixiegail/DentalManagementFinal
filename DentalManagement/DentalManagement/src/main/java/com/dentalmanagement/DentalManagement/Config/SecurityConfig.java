4d7bc6cc95e7e4350097cdbb9d59a0bf3e28bf00
trixie <trixiegail.cloma@cit.edu>
2024-11-14 23:37:47 +0800

    sec config

--- DentalManagement/DentalManagement/src/main/java/com/dentalmanagement/DentalManagement/Config/SecurityConfig.java
+++ DentalManagement/DentalManagement/src/main/java/com/dentalmanagement/DentalManagement/Config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
         provider.setUserDetailsService(userDetailsService);
 
 
