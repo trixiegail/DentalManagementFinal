c6b7f8815146eb1082626295a80f3a98cfaf3fa6
trixie <trixiegail.cloma@cit.edu>
2024-11-30 21:49:43 +0800

    approve email test

--- DentalManagement/DentalManagement/src/main/java/com/dentalmanagement/DentalManagement/Controller/EmailController.java
+++ DentalManagement/DentalManagement/src/main/java/com/dentalmanagement/DentalManagement/Controller/EmailController.java
@@ -4,6 +4,8 @@ import com.dentalmanagement.DentalManagement.Service.SendEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,17 @@ public class EmailController {
     public String sendEmail() {
         sendEmailService.sendEmail("dentalcapstone5@gmail.com", "This is a test email", "Test Email");
         return "Email sent successfully";
+    }
+
+    @PostMapping("/notify-approval")
+    public String notifyApproval(@RequestParam String email) {
+        sendEmailService.sendEmail(email, "Approval Notification", "You have been approved for your requested checkup.");
+        return "Approval email sent successfully";
+    }
+
+    @PostMapping("/notify-rejection")
+    public String notifyRejection(@RequestParam String email) {
+        sendEmailService.sendEmail(email, "Rejection Notification", "We regret to inform you that your request has been declined.");
+        return "Rejection email sent successfully";
     }
 }
