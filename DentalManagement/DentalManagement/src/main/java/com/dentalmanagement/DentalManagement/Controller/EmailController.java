c257e20e735cbff20111794636f6d656ba572f62
trixie <trixiegail.cloma@cit.edu>
2024-11-30 21:09:11 +0800

    email controller

--- DentalManagement/DentalManagement/src/main/java/com/dentalmanagement/DentalManagement/Controller/EmailController.java
+++ DentalManagement/DentalManagement/src/main/java/com/dentalmanagement/DentalManagement/Controller/EmailController.java
@@ -2,10 +2,14 @@ package com.dentalmanagement.DentalManagement.Controller;
 
 import com.dentalmanagement.DentalManagement.Service.SendEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/email")
+@CrossOrigin(origins = "https://projectyey.vercel.app/")
 public class EmailController {
 
     @Autowired
